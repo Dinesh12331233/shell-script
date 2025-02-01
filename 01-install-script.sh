bbaa0716658cec7544e9cb000c5fc59b1b495c19
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-02-01 10:03:39 +0530

    installation-of-mysql

--- 01-install-script.sh
+++ 01-install-script.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "you are not root user. you cannot install"
+    exit 1
+fi 
+ 
 dnf list installed mysql
 
 if [ $? -ne 0 ]
-then
+then 
     dnf install mysql -y
 
     if [ $? -ne 0 ]
@@ -12,7 +20,7 @@ then
         exit 1
     else
         echo "Installing MYSQL....SUCCESS"
-    fi 
+    fi         
 else
-    echo "MYSQL is already....INSTALLED"
-fi                  
+    echo "mysql is already....INSTALLED"
+fi         
