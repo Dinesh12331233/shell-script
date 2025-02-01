a12b13c5fad92d30a8d9c9d4e9477f5cbaea7a81
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-02-01 07:18:44 +0530

    installation-of-mysql

--- 01-install-script.sh
+++ 01-install-script.sh
@@ -2,10 +2,11 @@
 
 USERID=$(id -u)
 
-if [ $USERID -eq 0 ]
-then 
-    echo "user is root user"
-    dnf install mysql -y
+if [ $USERID -ne 0 ]
+then
+    echo "user is not root-user. hence we cannot install mysql"
 else
-    echo "To run install command user must be root-user" 
-fi       
+    echo "user is rrot-user"
+    dnf install mysqll -y
+fi
+    
