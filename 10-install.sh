0fd4528373f33701e48a76a254617d50f796e472
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-02-02 16:02:16 +0530

    rootuserOrNot

--- 10-install.sh
+++ 10-install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-dnf remove mysql -y
+#checking the user is root user or normal user
+
+USERID=$(id -u)
+
+if [ $USERID -eq 0 ]
+then
+    echo "you are root user. you can install"
+else
+    echo "you are normal user.you cannot install"
+    exit 1
+fi        
