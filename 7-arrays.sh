4d675eadaa538d4d25d9a3e9a7f64f152504cb44
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-02-02 11:49:04 +0530

    arrays

---
+++ 7-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("Pushpa" "RRR" "Devara" "Love_Today")
+
+echo "First movie is:${MOVIES[0]}"
+echo "Second movie is:${MOVIES[1]}"
+echo "Third movie is:${MOVIES[2]}"
+echo "Fourth movie is:${MOVIES[3]}"
