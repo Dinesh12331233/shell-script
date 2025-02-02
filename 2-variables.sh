a1d698c578d314fdafd0d5d4d4684d3b03c7c167
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-02-02 10:24:39 +0530

    variables

--- 2-variables.sh
+++ 2-variables.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
- 
-PERSON1=Deepthi
-PERSON2=Dinesh 
 
-echo "$PERSON1::  Hello $PERSON2"
-echo "$PERSON2:: Hi chinni $PERSON1"
+echo "please enter name of PERSON1:"
+read PERSON1
+
+echo "please enter name of PERSON2:"
+read PERSON2
+
+echo "$PERSON1:: Hello $PERSON2"
+echo "$PERSON2:: Hi $PERSON1"
 echo "$PERSON1:: How do you do $PERSON2"
-echo "$PERSON2:: I am good. How are you chinni $PERSON1"
+echo "$PERSON2:: I am good.How are you doing $PERSON1"
