4997aebd7173d3f8ddf7cb9256f83af21a0218de
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-02-02 11:26:11 +0530

    runtime-variables-addition

--- 6-datatypes.sh
+++ 6-datatypes.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-NUM1=$1
-NUM2=$2
-NUM3=$3
-NUM4=$4
-
-SUM=$(($NUM1+$NUM2+$NUM3+$NUM4))
+SUM=$(($1+$2+$3+$4))
 
 TIMESTAMP=$(date)
 
-echo "Sum of $NUM1,$NUM2,$NUM3 and $NUM4 is $SUM"
+echo "Sum of $1,$2,$3 and $4 is $SUM"
 
 echo "script is executed at:$TIMESTAMP"
 
