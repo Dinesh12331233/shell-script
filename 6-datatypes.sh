3a6ed15b521bc97dd4c7d6cbd17cd8784aceeca6
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-02-02 11:15:19 +0530

    UserInutVariables-Timestamp

--- 6-datatypes.sh
+++ 6-datatypes.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-SUM=$(($1+$2+$3+$4))
+echo "please enter first number:"
+read NUM1
 
-echo "Sum of $1,$2,$3 and $4 is $SUM"
+echo "please enter second number:"
+read NUM2
+
+echo "please enter third number:"
+read NUM3
+
+SUM=$(($NUM1+$NUM2+$NUM3))
+
+TIMESTAMP=$(date)
+
+echo "sum of $NUM1,$NUM2 and $NUM3 is $SUM"
+
+echo "Script executed at:$TIMESTAMP"
