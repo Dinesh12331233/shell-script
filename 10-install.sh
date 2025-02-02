f012defb198987c89be3ccf63b5324f2f4ebe776
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-02-02 16:50:47 +0530

    mysql and git installation

--- 10-install.sh
+++ 10-install.sh
@@ -1,12 +1,37 @@
-#!/bin/bash
+#!/bin/bash 
 
-ls -la
+#check the user is root-user or normal-user
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "you are normal-user. you cannot install"
+    exit 1
+else 
+    echo "you are root-user.you can proceed further"
+fi 
+
+dnf install mysqll -y
+
+#write an exit status for this command
 
 if [ $? -ne 0 ]
 then 
-    echo "previous command is FAILURE"
+    echo "Installing mysql....FAILURE"
     exit 1
-else
-    echo "previous command is SUCCESS"
+else 
+    echo "Installing mysql....SUCCESS"
 fi 
-        
+
+dnf install gitt -y
+
+#write an exit status for this command
+
+if [ $? -ne 0 ]
+then 
+    echo "Installing git....FAILURE"
+    exit 1
+else 
+    echo "Installing git....SUCCESS"
+fi        
