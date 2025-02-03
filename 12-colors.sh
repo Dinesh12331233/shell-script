4fb262b8b342bd7ba4d44d80bf75d2c61c38b7de
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-02-03 07:32:13 +0530

    colors

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,9 +1,51 @@
 #!/bin/bash
 
-echo -e "Hello World"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 
-echo -e "\e[31mHello Wolrd"
 
-echo -e "\e[32mHello World"
+#check the user is root-user or not. if the user is normal-user then exit from the script
 
-echo -e "\e[33mHello World"
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....$R FAILURE"
+        exit 1
+    else 
+        echo -e "$2....$G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "you are normal-user. you cannot install"
+    exit 1
+fi 
+
+#checking whether mysql is already installed or not 
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    VALIDATE $? "Installing MYSQL"             
+else 
+    echo -e "MYSQL is already....$Y INSTALLED" 
+fi 
+
+#checking whether git is already installed or not
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+             
+else 
+    echo -e "Git is already....$Y INSTALLED"
+fi 
